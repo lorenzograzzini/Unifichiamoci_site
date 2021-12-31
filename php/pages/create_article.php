361d0f1ec2606e38c9d569fea58f88daaeebddf7
lorenzograzzini <74548411+lorenzograzzini@users.noreply.github.com>
2021-12-31 18:38:50 +0100

    Create create_article.php

---
+++ php/pages/create_article.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Unifichiamoci</title>
+    <link rel="stylesheet" href="../../css/collaborazioni.css" type="text/css">
+    <link href="../../css/style.css" rel="stylesheet"type="text/css">
+  </head>
+  <body style="margin: 0;">
+    <?php
+  	echo file_get_contents("../header.php");
+  	 ?>
+    <iframe src="../footer.html" width="100%" height="142px;" style="border: none; margin-top: 10px;"></iframe>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script></div>
+  </body>
+</html>
